a8af22f653c8b9e783e996c41415ee9852e69a46
Faruk Yucel <fby1@pitt.edu>
2019-10-23 14:58:09 -0400

    Added air resistance to projectile motion demo

--- projectile_motion_demo.cpp
+++ projectile_motion_demo.cpp
@@ -3,8 +3,13 @@
 #include <thread>
 #include <math.h>
 
+// Math constants
 const int M_PI = atan(1) * 4;
 const int radian_to_degree = 180.0 / M_PI;
+
+// Increase value to have projectile fall slower and have less air resistance (drag)
+// Decrease to have projectile fall faster and have more air resistance
+const int air_time_divisor = 3;
 
 class Projectile {
 	public:
@@ -19,14 +24,28 @@ class Projectile {
 
 		int x_velocity()
 		{
-			return velocity * cos(pitch * radian_to_degree);
+			// Decrease x velocity to simulate air resistance,
+			// but half as slowly as the decrease in y velocity
+			int adjustment = air_time / (2 * air_time_divisor);
+			int x_velocity = velocity * cos(pitch * radian_to_degree);
+
+			// Prevent projectile from moving backwards in the x-direction
+			int adjusted_x_velocity = x_velocity - adjustment;
+			if (adjusted_x_velocity <= 0)
+			{
+				adjusted_x_velocity = 0;
+			}
+
+			return adjusted_x_velocity;
 		}
 
-		// Decreases the y velocity by the air time to simulate bullet drop
-		// May have to experience with the divisor
 		int y_velocity()
 		{
-			return velocity * sin(pitch * radian_to_degree) - (air_time / 3);
+			// Decreases the y velocity by the air time to simulate bullet drop
+			int adjustment = air_time / air_time_divisor;
+			int y_velocity = velocity * sin(pitch * radian_to_degree);
+
+			return y_velocity - adjustment;
 		}
 
 		// Accessor methods
@@ -40,9 +59,9 @@ class Projectile {
 			return y_position;
 		}
 
-		int get_air_time()
+		int get_pitch()
 		{
-			return air_time;
+			return pitch;
 		}
 
 	private:
@@ -54,7 +73,7 @@ void output_info(Projectile* projectile)
 {
 	std::cout << "(" << projectile->get_x_position() << ", " << projectile->get_y_position() << ")";
 	std::cout << " - " << projectile->x_velocity() << "/" << projectile->y_velocity();
-	std::cout << " [" << projectile->get_air_time() << "]" << std::endl;
+	std::cout << " [" << projectile->get_pitch() << "]" << std::endl;
 }
 
 void wait_milliseconds(int amount)
@@ -64,7 +83,7 @@ void wait_milliseconds(int amount)
 
 int main()
 {
-	int x_position = 0, y_position = 0, velocity = 5, pitch = 220;
+	int x_position = 0, y_position = 0, velocity = 5, pitch = 0;
 	Projectile* projectile = new Projectile(x_position, y_position, velocity, pitch);
 
 	std::cout << "( x position, y position) - x velocity / y velocity [ pitch ]" << std::endl;
