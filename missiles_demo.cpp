ba1c1d7024cd58da59b9bd78ab5863db05f4f8a7
Yucel <FBY1@pitt.edu>
2019-11-06 12:31:39 -0500

    Added missile class to missiles demo

--- missiles_demo.cpp
+++ missiles_demo.cpp
@@ -10,41 +10,88 @@ const double degree_to_radian = atan(1) * 4 / 180.0;
 
 class Plane
 {
-public:
-	Plane(double hit_points, double x_position, double y_position,
-		double x_velocity, double y_velocity, double direction) :
-		hit_points{ hit_points }, x_position{ x_position }, y_position{ y_position },
-		x_velocity{ x_velocity }, y_velocity{ y_velocity }, direction{ direction } {}
+	public:
+		Plane(double hit_points, double x_position, double y_position,
+			double x_velocity, double y_velocity, double direction) :
+			hit_points{ hit_points }, x_position{ x_position }, y_position{ y_position },
+			x_velocity{ x_velocity }, y_velocity{ y_velocity }, direction{ direction } {}
 
-	// Amount of damage the plane can endure before it is destroyed
-	double hit_points;
+		// Amount of damage the plane can endure before it is destroyed
+		double hit_points;
 
-	// Position coordinates
-	double x_position, y_position;
+		// Position coordinates
+		double x_position, y_position;
 
-	// Velocity components
-	double x_velocity, y_velocity;
+		// Velocity components
+		double x_velocity, y_velocity;
 
-	// Direction angle in degrees
-	double direction;
+		// Direction angle in degrees
+		double direction;
 
-	// Deals damage to the plane's hitpoints
-	double deal_damage(double damage)
-	{
-		if (damage >= hit_points)
+		// Deals damage to the plane's hitpoints
+		double deal_damage(double damage)
 		{
-			hit_points = 0;
+			if (damage >= hit_points)
+			{
+				hit_points = 0;
+			}
+			else
+			{
+				hit_points -= damage;
+			}
 		}
-		else
-		{
-			hit_points -= damage;
-		}
-	}
 
-	void move()
-	{
-		x_position += x_velocity * cos(direction * degree_to_radian);
-		y_position += y_velocity * sin(direction * degree_to_radian);
-	}
+		void move()
+		{
+			x_position += x_velocity * cos(direction * degree_to_radian);
+			y_position += y_velocity * sin(direction * degree_to_radian);
+		}
 };
 
+class Missile
+{
+	public:
+		Missile(double damage, double x_position, double y_position,
+			double x_velocity, double y_velocity, double direction, double blast_radius) :
+			damage{ damage }, x_position{ x_position }, y_position{ y_position },
+			x_velocity{ x_velocity }, y_velocity{ y_velocity }, direction{ direction },
+			blast_radius{ blast_radius } {}
+
+		// Amount of damage the warhead deals at the center of the blast radius
+		double damage;
+
+		// Position coordinates
+		double x_position, y_position;
+
+		// Velocity components
+		double x_velocity, y_velocity;
+
+		// Direction angle in degrees
+		double direction;
+
+		// Maximum distance the warhead can cause damage
+		double blast_radius;
+
+		// Calculates how far the entity is to this missile,
+		// using the basic distance formula for two points in a X and Y plane
+		double calculate_distance(int entity_x, int entity_y)
+		{
+			return abs(sqrt(pow(x_position - entity_x, 2) + pow(y_position - entity_y, 2)));
+		}
+
+		// Calculates how much damage the warhead will do to entity,
+		// depending on the entity's distance to the warhead
+		// Using a very simple rational function for now
+		double calculate_damage(int entity_x, int entity_y)
+		{
+			double distance = calculate_distance(entity_x, entity_y);
+
+			return damage / distance;
+		}
+
+		void move()
+		{
+			x_position += x_velocity * cos(direction * degree_to_radian);
+			y_position += y_velocity * sin(direction * degree_to_radian);
+		}
+};
