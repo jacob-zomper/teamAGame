3e50c47ebc7be40aa1b23c16a5ff74612922f11e
fby1 <fby1@pitt.edu>
2019-11-07 15:02:18 -0500

    Made outline for class

---
+++ missile.cpp
@@ -0,0 +1,61 @@
+#include <missile.h>
+#include <SDL.h>
+#include <iostream>
+#include <cmath>
+
+Missile::Missile() {}
+
+void Missile::renderMissile(SDL_Renderer* gRenderer)
+{
+
+}
+
+void Missile::move()
+{
+
+}
+
+double Missile::calculate_distance(double entity_x, double entity_y)
+{
+
+}
+
+double Missile::calculate_damage(double entity_x, double entity_y)
+{
+
+}
+
+// Accessor methods
+
+int Missile::getX()
+{
+
+}
+
+int Missile::getY()
+{
+
+}
+
+int Missile::getWidth()
+{
+
+}
+
+int Missile::getHeight()
+{
+
+}
+
+int Missile::getXVel()
+{
+
+}
+
+int Missile::getYVel()
+{
+
+}
+
+
+
