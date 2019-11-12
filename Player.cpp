262b6f20f1a4e412706bdcfcad4f0b401c6dbce2
Tera Coder <TerableCoder@gmail.com>
2019-11-12 11:20:47 -0500

    smoothed out y deceleration animation

--- Player.cpp
+++ Player.cpp
@@ -121,7 +121,9 @@ void Player::move(int SCREEN_WIDTH, int SCREEN_HEIGHT, int LEVEL_HEIGHT, int cam
     float accelerate_by = 0.003*time_since_move;
     float deccelerate_factor = 4.0;
     acceleration(yp_decel, yn_decel, y_accel, accelerate_by, deccelerate_factor, y_vel);
-    tiltAngle = 180 * sin(y_accel / 12);
+    if(!yp_decel && !yn_decel && y_vel > 0) tiltAngle = 180 * sin(y_accel / 12) > 0 ? 180 * sin(y_accel / 12) : 0;
+    else if(!yp_decel && !yn_decel && y_vel < 0) tiltAngle = -180 * sin(y_accel / 12) < 0 ? -180 * sin(y_accel / 12) : 0;
+    else tiltAngle = 180 * sin(y_accel / 12);
     acceleration(xp_decel, xn_decel, x_accel, accelerate_by, deccelerate_factor, x_vel);
 
     if (y_vel > MAX_PLAYER_VEL)
