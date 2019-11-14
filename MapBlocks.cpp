525f6b357efb7ba937a1e99589271cd865c89626
Matt Darden <mdarden.pitt19@gmail.com>
2019-11-13 21:57:45 -0500

    Kam not active during randomly gen'd cave

--- MapBlocks.cpp
+++ MapBlocks.cpp
@@ -170,19 +170,19 @@ Missile * Turret::handleFiring(int posX, int posY) {
 
 	time_since_move = SDL_GetTicks() - last_move;
 	Missile * m = nullptr;
-	if (time_since_move >= SHOOT_FREQ) {
-		last_move = SDL_GetTicks();
-		int xDist = posX - BLOCK_REL_X;
-		int yDist = posY - BLOCK_REL_Y;
-		double math = (double)xDist / sqrt(xDist * xDist + yDist * yDist) * 400;
-		double math2 = ((double)yDist / sqrt(xDist * xDist + yDist * yDist)) * 400;
-		if (BLOCK_REL_Y >= posY){
-			m = new Missile(damage, blast_radius, BLOCK_REL_X + BLOCK_WIDTH / 2, BLOCK_REL_Y - 20, ((double)xDist / sqrt(xDist * xDist + yDist * yDist)) * 400, ((double)yDist / sqrt(xDist * xDist + yDist * yDist)) * 400, gRenderer);
-		}
-		else {
-			m = new Missile(damage, blast_radius, BLOCK_REL_X + BLOCK_WIDTH / 2, BLOCK_REL_Y + 5 + BLOCK_HEIGHT, ((double)xDist / sqrt(xDist * xDist + yDist * yDist)) * 400, ((double)yDist / sqrt(xDist * xDist + yDist * yDist)) * 400, gRenderer);
-		}
-	}
+	// if (time_since_move >= SHOOT_FREQ) {
+	// 	last_move = SDL_GetTicks();
+	// 	int xDist = posX - BLOCK_REL_X;
+	// 	int yDist = posY - BLOCK_REL_Y;
+	// 	double math = (double)xDist / sqrt(xDist * xDist + yDist * yDist) * 400;
+	// 	double math2 = ((double)yDist / sqrt(xDist * xDist + yDist * yDist)) * 400;
+	// 	if (BLOCK_REL_Y >= posY){
+	// 		m = new Missile(damage, blast_radius, BLOCK_REL_X + BLOCK_WIDTH / 2, BLOCK_REL_Y - 20, ((double)xDist / sqrt(xDist * xDist + yDist * yDist)) * 400, ((double)yDist / sqrt(xDist * xDist + yDist * yDist)) * 400, gRenderer);
+	// 	}
+	// 	else {
+	// 		m = new Missile(damage, blast_radius, BLOCK_REL_X + BLOCK_WIDTH / 2, BLOCK_REL_Y + 5 + BLOCK_HEIGHT, ((double)xDist / sqrt(xDist * xDist + yDist * yDist)) * 400, ((double)yDist / sqrt(xDist * xDist + yDist * yDist)) * 400, gRenderer);
+	// 	}
+	// }
 	return m;
 }
 
